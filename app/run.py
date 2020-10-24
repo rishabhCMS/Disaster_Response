ffd87edbef473da7791676c8824c070351e13c5e
--replace-all <rishabh.uniyal@cern.ch>
2020-10-24 16:56:51 -0400

    adding the project

---
+++ app/run.py
@@ -0,0 +1,132 @@
+import json
+import plotly
+import pandas as pd
+
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+from flask import Flask
+from flask import render_template, request, jsonify
+from plotly.graph_objs import Bar
+import joblib
+from sqlalchemy import create_engine
+
+from collections import Counter
+
+app = Flask(__name__)
+
+def tokenize(text):
+    ''' 
+    
+    A tokenization function to process text data 
+    
+    Parameters:
+    text: a string with untokenizated sentences
+    Returns:
+    clean_tokens: a list of tokenization words from input sentences
+    '''
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+# load data
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('disasterresponse_etl', engine)
+
+# load model
+model = joblib.load("../models/classifier.pkl")
+
+
+# index webpage displays cool visuals and receives user input text for model
+@app.route('/')
+@app.route('/index')
+def index():
+    
+    # extract data needed for visuals
+    # TODO: Below is an example - modify to extract data for your own visuals
+    genre_counts = df.groupby('genre').count()['message']
+    genre_names = list(genre_counts.index)
+    
+    # extract Top 5 categories
+    remove_col = ['id', 'message', 'original', 'genre']
+    y = df.loc[:, ~df.columns.isin(remove_col)]
+    category_counts = y.sum().sort_values().tail()
+    category_names = list(category_counts.index)
+
+    # create visuals
+    # TODO: Below is an example - modify to create your own visuals
+    graphs = [
+        {
+            'data': [
+                Bar(
+                    x=genre_names,
+                    y=genre_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Genres',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Genre"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    y=category_names,
+                    x=category_counts,
+                    orientation = 'h'
+                )
+            ],
+
+            'layout': {
+                'title': 'Top 5 Categories',
+                'xaxis': {
+                    'title': "Count"
+                }
+            }
+        }
+    ]
+    
+    # encode plotly graphs in JSON
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    # render web page with plotly graphs
+    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+
+
+# web page that handles user query and displays model results
+@app.route('/go')
+def go():
+    # save user input in query
+    query = request.args.get('query', '') 
+
+    # use model to predict classification for query
+    classification_labels = model.predict([query])[0]
+    classification_results = dict(zip(df.columns[4:], classification_labels))
+
+    # This will render the go.html Please see that file. 
+    return render_template(
+        'go.html',
+        query=query,
+        classification_result=classification_results
+    )
+
+
+def main():
+    app.run(host='0.0.0.0', port=3003, debug=True)
+
+
+if __name__ == '__main__':
+    main()
---
+++ models/train_classifier.py
@@ -0,0 +1,172 @@
+# import libraries
+import sys
+
+import nltk
+nltk.download(['punkt', 'wordnet', 'stopwords'])
+
+from sqlalchemy import create_engine
+import pandas as pd
+import numpy as np
+import re
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+
+import pickle
+
+def load_data(database_filepath):
+    ''' 
+    this functions loads the data from a the sql db file
+
+    args: 
+        database_filepath: location of the db file
+
+    Returns:
+    X: the message column
+    y: the categories
+    category_name: the names of the categories
+    '''
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    df = pd.read_sql_table(table_name='disasterresponse_etl', con=engine)
+    X = df.message.values
+    remove_col = ['id', 'message', 'original', 'genre']
+    y = df.loc[:, ~df.columns.isin(remove_col)]
+    y.loc[:,'related'] = y['related'].replace(2,1)
+    category_name = y.columns
+    return X, y, category_name
+
+url_regex = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+
+def tokenize(text):
+    ''' 
+    
+    this function applies the nlp tokenization 
+    
+    args:
+    text: a string with untokenizated sentences
+    Returns:
+    clean_tokens: a list of tokenization words from input sentences
+    '''
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+
+def build_model():
+    '''
+    
+    A machine learning pipeline takes in the message column as input and 
+    output classification results on the other 36 categories in the dataset. 
+    Parameters:
+    None
+    Returns:
+    cv: a model that uses the message column to predict classifications for 36 categories
+    '''
+    pipeline = Pipeline([
+        ('features', FeatureUnion([
+            ('text_pipeline', Pipeline([
+                ('vect', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf', TfidfTransformer())
+            ]))
+        ])),
+        
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+
+    parameters = {
+        'features__text_pipeline__tfidf__use_idf': (True, False),
+#         'clf__estimator__n_estimators': [50, 100, 200],
+        'clf__estimator__min_samples_split': [2, 3, 4]
+    }
+    
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+
+    return cv
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    ''' 
+    
+    evaluates model performance based on 
+    a. f1score b. precesion and recall
+
+    args:
+        model: the model to be evaluated
+        X_test: test dataset (messages tokenized list)
+        y_test: categories for each message
+        category_names: list of categories for the messages to be classified
+
+    prints the classification report 
+    '''
+
+    y_pred = model.predict(X_test)
+    Y_pred = pd.DataFrame(data=y_pred, 
+                          index=Y_test.index, 
+                          columns=category_names)
+    print(classification_report(Y_test, Y_pred, target_names=category_names))
+
+
+def save_model(model, model_filepath):
+    '''
+    
+    Save the model to a specified path
+    
+    args:
+    model: ML model
+    model_filepath: file path for saving the model
+    Returns: none
+
+    '''
+    pickle.dump(model, open(model_filepath, 'wb'))
+
+
+def main():
+    '''
+    
+    args:
+    arg1: the file path of the database
+    arg2: the file path where the trained model will be saved
+    Returns:
+    '''
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data from db')
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test,category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
